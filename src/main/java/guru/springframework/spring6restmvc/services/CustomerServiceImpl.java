15b2bbd6cc1f86b20fec006df4317a6428ca8d3b
jcmartinez <jcmartinez@softtek.com>
2023-04-24 15:42:56 +0200

    tarea 8

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -83,4 +83,9 @@ public class CustomerServiceImpl implements CustomerService {
 
         customerMap.put(existing.getId(), existing);
     }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        customerMap.remove(customerId);
+    }
 }
