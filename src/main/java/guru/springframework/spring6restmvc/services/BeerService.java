45148a18a8ab82a3e17d0a3485df518c34216312
jcmartinez <jcmartinez@softtek.com>
2023-04-24 10:15:36 +0200

    video 53

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+
+import java.util.UUID;
+
+public interface BeerService {
+
+    Beer getBeerById(UUID id);
+}
+
+
+
+
