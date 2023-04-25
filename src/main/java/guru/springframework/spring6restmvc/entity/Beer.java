fd32ba55f56cf7e0d6ce97b3569d9de966329b1c
jcmartinez <jcmartinez@softtek.com>
2023-04-25 15:27:41 +0200

    video 96

--- src/main/java/guru/springframework/spring6restmvc/entity/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entity/Beer.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.UUIDGenerator")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     @Version
