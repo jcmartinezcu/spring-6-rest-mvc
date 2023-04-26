18a9ddc370d3090381b7f61b6eeae4a4449a21c7
jcmartinez <jcmartinez@softtek.com>
2023-04-26 13:01:46 +0200

    video 114

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entity.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,8 +22,12 @@ class BeerRepositoryTest {
     void testSavedBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
+                        .beerStyle(BeerStyle.ALE)
+                        .upc("23232323232")
+                        .price(new BigDecimal("11.99"))
                 .build());
 
+        beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
     }
